b4f4313dd90318ad01d04a4b2c5ce9559035d73d
Andreas Marek <andimarek@fastmail.fm>
2021-01-18 15:09:48 +1100

    apollo subscription call

--- src/index.js
+++ src/index.js
@@ -3,6 +3,75 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import gql from "graphql-tag";
+import { createClient } from "graphql-ws";
+import { ApolloLink, Observable, ApolloClient, InMemoryCache } from '@apollo/client';
+import { print } from 'graphql';
+
+class WebSocketLink extends ApolloLink {
+
+  constructor(options) {
+    super();
+    this.client = createClient(options);
+  }
+
+  request(operation) {
+    return new Observable((sink) => {
+      console.log('subscribe: ',{ ...operation, query: print(operation.query)});
+      return this.client.subscribe(
+        { ...operation, query: print(operation.query) },
+        {
+          next: sink.next.bind(sink),
+          complete: sink.complete.bind(sink),
+          error: (err) => {
+            console.log('error: ', err);
+            if (err instanceof Error) {
+              sink.error(err);
+            } else if (err instanceof CloseEvent) {
+              sink.error(
+                new Error(
+                  `Socket closed with event ${err.code}` + err.reason
+                    ? `: ${err.reason}` // reason will be available on clean closes
+                    : '',
+                ),
+              );
+            } else {
+              sink.error(
+                new Error(
+                  err
+                    .map(({ message }) => message)
+                    .join(', '),
+                ),
+              );
+            }
+          },
+        },
+      );
+    });
+  }
+}
+
+const wsLink = new WebSocketLink({
+  url: 'ws://localhost:8080/graphql/websocket'
+});
+
+
+const client = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache()
+});
+
+
+
+client
+  .subscribe({
+    query: gql`
+      subscription {
+        newPeopleAdded {id name}
+      }
+    `
+  })
+  .subscribe(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
